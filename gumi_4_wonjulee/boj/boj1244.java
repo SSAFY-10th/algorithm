3585a2c36daac00407fa318ccfa5802bf05f9f35
이원주 <turtle4265@naver.com>
2023-08-03 12:02:23 +0900

    [fix] 코드 리뷰 후 수정사항 반영

--- gumi_4_wonjulee/boj/boj1244.java
+++ gumi_4_wonjulee/boj/boj1244.java
@@ -14,18 +14,13 @@ public class Main {
 		arr = new int[n];
 		
 		st = new StringTokenizer(br.readLine());
-		// 스위치 상태 입력받기
 		for(int i=0; i<n; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
-		// input end
 		
-		
-		// 학생들의 키 정보 받을 횟수
 		int m = Integer.parseInt(br.readLine());
 		
 		int []data = new int[2];
-		
 		for(int i=0; i<m; i++) {
 			st = new StringTokenizer(br.readLine());
 			for(int j=0; j<2; j++) {
@@ -39,39 +34,33 @@ public class Main {
 				arr = girl(data[1]);
 			}
 		}
-		int cnt = 1;
 		
-		// 출력
+		StringBuilder sb = new StringBuilder();
+		
 		for(int i=0; i<arr.length; i++) {
-			if(i == 20*cnt) {
-				System.out.println();
-				cnt++;
-			}
-			System.out.print(arr[i] +" ");
+			sb.append(arr[i]+" ");
+			if((i+1)% 20 == 0) sb.append("\n"); 
 		}
+		System.out.println(sb);
 	}
 	
-	// 남학생 함수
 	public static int[] boy(int data) {
-		for (int i = 0; i < arr.length; i++) {
-			if ((i + 1) % data == 0) {
-				arr[i] = arr[i] == 0 ? 1 : 0;
-			}
+		for (int i = data-1; i < arr.length; i+=data) {
+			arr[i] ^= 1;
 		}
 		return arr;
 	}
 	
-	// 여학생 함수
 	public static int[] girl(int data) {
 		data--;
-		arr[data] = arr[data] == 1 ? 0 : 1;
+		arr[data] ^= 1;
 		for(int i=1; i<=data; i++) {
 			if(data+i >= arr.length || data-i < 0) {
 				break;
 			}
 			if(arr[data+i] == arr[data-i]) {
-				arr[data+i] = arr[data+i] == 1 ? 0 : 1;
-				arr[data-i] = arr[data-i] == 1 ? 0 : 1;
+				arr[data+i] ^= 1;
+				arr[data-i] ^= 1;
 			} else {
 				return arr;
 			}
